ad069c33b78522719364cef3d931b8df3e4c60e0
isandratskiy <i.sandrackii@gmail.com>
2020-03-16 04:25:25 +0200

    initial

---
+++ service_start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo " ==============================> pulling Chrome " 
+docker pull selenoid/vnc_chrome:80.0
+echo " starting Selenoid Service ....... "
+echo " starting Jenkins ................ "
+docker-compose up --force-recreate -d
